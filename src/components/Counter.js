3f6a63a00eff0a295427d6eec7cac92e3c26df66
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-09-09 19:40:58 +0600

    add

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  decrementCounter,
+  incrementCounter,
+  reset,
+} from '../services/actions/counterAction';
 
 const Counter = () => {
+  const count = useSelector((state) => state.count);
+  // console.log(count);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(incrementCounter());
+  };
+  const handleDecrement = () => {
+    dispatch(decrementCounter());
+  };
+  const handleReset = () => {
+    dispatch(reset());
+  };
+
   return (
     <>
-      <h1>Count</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleReset}>0</button>
+      <button onClick={handleDecrement}>-</button>
     </>
   );
 };
