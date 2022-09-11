b24af85ca09fd128653051a6c051fa65d5c1c2ae
Syed Monir <Rambling.ctg@gmail.com>
2022-09-11 11:27:06 +0600

    counter app

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,6 +9,7 @@ import {
 const Counter = () => {
   const count = useSelector((state) => state.count);
   // console.log(count);
+
   const dispatch = useDispatch();
 
   const handleIncrement = () => {
@@ -32,3 +33,12 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// 1. state - count : 0
+// 2. actions increment, decrement, reset
+// 3. reducers increment count ⇒count + 1
+// decrement → count ⇒ count - 1
+// reset → count 0
+// 4. store
+// 5. providing store in react
+// 6. use store
